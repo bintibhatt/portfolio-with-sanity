b1c6f9af0953c58e4ae8e003755748592e6e88e4
Binti Bhatt <bintibhatt.zuru@gmail.com>
2024-01-18 17:15:59 +0530

    Refactor AchievementsComp.js and Footer.js

--- next-frontend/src/app/components/AchievementsComp.js
+++ next-frontend/src/app/components/AchievementsComp.js
@@ -1,72 +1,31 @@
 import React from "react";
 import { createClient } from "next-sanity";
+import Link from "next/link";
 
 export default async function AchievementsPage() {
-  const client = createClient({
-    projectId: "qb0ilyn2",
-    dataset: "production",
-    useCdn: false,
-  });
-
-  const experiences = await client.fetch(
-    `*[_type == "experience"] | order(_createdAt desc) `,
-    {
-      cache: "no-cache",
-    }
-  );
-  const educations = await client.fetch(
-    `*[_type == "education"] | order(_createdAt desc) `,
-    {
-      cache: "no-cache",
-    }
-  );
-
-  console.log(experiences);
   return (
-    <div className="flex justify-around h-full bg-gray-600 p-16 rounded-b-3xl">
+    <div className="flex h-full bg-gray-600 bg-opacity-60 p-16 rounded-b-3xl">
       <div>
-        <h1 className="text-3xl font-bold mb-8">My Experiences</h1>
-        <ol className="relative border-s border-gray-200 dark:border-gray-700">
-          {experiences.map((experience) => (
-            <li className="mb-10 ms-4">
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-md font-normal leading-none text-gray-300 dark:text-gray-300">
-                {experience.date}
-              </time>
-              <div className="flex items-center mb-2 text-lg font-semibold leading-none text-gray-200 dark:text-gray-300">
-                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                  {experience.title} -{" "}
-                </h3>
-                <p className="text-md font-normal text-gray-500 dark:text-gray-300 mt-1 ml-2">
-                  {experience.company}
-                </p>
+        <h1 className="text-3xl font-bold mb-8">My Achievements</h1>
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-8 text-justify">
+          <div className="p-4">
+            <a
+              href="#"
+              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+            >
+              <img
+                className="object-contain w-full rounded-t-lg h-32  md:w-48 md:rounded-none md:rounded-s-lg"
+                src="/assets/images/MLSA.png"
+                alt=""
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Microsoft Learn Student Ambassador
+                </h5>
               </div>
-              <p className="text-base font-normal text-gray-500 dark:text-gray-300">
-                {experience.skills}
-              </p>
-            </li>
-          ))}
-        </ol>
-      </div>
-      <div>
-        <h1 className="text-3xl font-bold mb-8">My Education</h1>
-        <ol className="relative border-s border-gray-200 dark:border-gray-700">
-          {educations.map((education) => (
-            <li className="mb-10 ms-4">
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-              <time className="mb-1 text-md font-normal leading-none text-gray-300 dark:text-gray-300">
-                {education.date}
-              </time>
-              s
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                {education.degree}
-              </h3>
-              <p className="text-md font-normal text-gray-500 dark:text-gray-300">
-                {education.institution}
-              </p>
-            </li>
-          ))}
-        </ol>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
--- next-frontend/src/app/components/Footer.js
+++ next-frontend/src/app/components/Footer.js
@@ -3,19 +3,17 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <footer className="text-center text-gray-100 body-font">
-        <div className="container px-5 py-8 mx-auto flex items-center justify-center sm:flex-row flex-col">
-          <p className="text-sm text-gray-100  sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
-            © 2024 Portfolio —
-            <a
-              href="https://linkedin.com/in/bintibhatt"
-              className="text-gray-100  ml-1"
-              target="_blank"
-            >
-              @bintibhatt
-            </a>
-          </p>
-        </div>
+      <footer className="fixed bottom-0 left-0 z-20 w-full p-1 bg-gray-700 shadow md:flex md:items-center md:justify-between md:p-2 0 bg-opacity-50">
+        <span className="text-sm text-gray-200 sm:text-center dark:text-gray-200 text-center">
+          © 2024{" "}
+          <a
+            href="https://linkedin.com/in/bintibhatt"
+            className="hover:underline"
+          >
+            @bintibhatt
+          </a>
+          . All Rights Reserved.
+        </span>
       </footer>
     </div>
   );
